14b7a00bc55e1e8f2e112cfe8d6654743e15b80f
Rick Vu Truong <ricktruong@Ricks-MacBook-Pro.local>
2022-02-13 00:01:03 -0800

    Restructured Derek Branch to match main project structure

--- app/src/main/java/com/example/birdsofafeather/PhotoActivity.java
+++ app/src/main/java/com/example/birdsofafeather/PhotoActivity.java
@@ -26,6 +26,6 @@ public class PhotoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_picture);
+        setContentView(R.layout.activity_photo);
     }
 }
